eec69c8333ebc9ec7f1afee4f2e4fd731837941a
damans227 <damans227@gmail.com>
2022-06-01 20:11:23 -0400

    extending instance data-source to use its id as an input

--- cloudstack/data_source_cloudstack_instance.go
+++ cloudstack/data_source_cloudstack_instance.go
@@ -20,16 +20,95 @@
 package cloudstack
 
 import (
+	"log"
+
+	"github.com/apache/cloudstack-go/v2/cloudstack"
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
 func dataSourceCloudstackInstance() *schema.Resource {
 	return &schema.Resource{
-		Read:   dataSourceCloudstackInstanceRead,
-		Schema: map[string]*schema.Schema{},
+		Read: dataSourceCloudstackInstanceRead,
+		Schema: map[string]*schema.Schema{
+
+			"instance_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+
+			// Computed values
+			"account": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"display_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"state": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"host_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"zone_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"created": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			// "ip_address": {
+			// 	Type:     schema.TypeString,
+			// 	Computed: true,
+			// },
+		},
 	}
 }
 
 func dataSourceCloudstackInstanceRead(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("Instance Data Source Read Started")
+
+	cs := meta.(*cloudstack.CloudStackClient)
+	p := cs.VirtualMachine.NewListVirtualMachinesParams()
+	instance_id := d.Get("instance_id").(string)
+
+	log.Printf("Instance ID =====================> %v", instance_id)
+
+	csInstances, _ := cs.VirtualMachine.ListVirtualMachines(p)
+
+	for _, instance := range csInstances.VirtualMachines {
+		log.Printf("Instance ===============> %v", instance)
+		if instance.Id == instance_id {
+
+			d.Set("instance_id", instance.Id)
+			d.Set("account", instance.Account)
+			d.Set("created", instance.Created)
+			d.Set("display_name", instance.Displayname)
+			d.Set("state", instance.State)
+			d.Set("host_id", instance.Hostid)
+			d.Set("zone_id", instance.Zoneid)
+
+			log.Printf("=========================================================================")
+
+			log.Printf("Instance-ID being Set To: ===============> %v", d.Get("instance_id").(string))
+			log.Printf("Instance-Created being Set To: ===============> %v", d.Get("created").(string))
+			log.Printf("Instance-Display-Name being Set To: ===============> %v", d.Get("display_name").(string))
+			log.Printf("Instance-State being Set To: ===============> %v", d.Get("state").(string))
+			log.Printf("Instance-Host-ID being Set To: ===============> %v", d.Get("host_id").(string))
+			log.Printf("Instance-Zone-ID being Set To: ===============> %v", d.Get("zone_id").(string))
+
+			log.Printf("=========================================================================")
+		}
+	}
+	log.Printf("Instance Data Source Read Ended")
+
 	return nil
 }
