1c9dfbbf2625a99606093d26aa8ee73b701a76c1
damans227 <damans227@gmail.com>
2022-06-03 20:24:21 -0400

    implement filters to allow filtering off of any exported attributes

--- cloudstack/data_source_cloudstack_instance.go
+++ cloudstack/data_source_cloudstack_instance.go
@@ -20,7 +20,12 @@
 package cloudstack
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"regexp"
+	"strings"
+	"time"
 
 	"github.com/apache/cloudstack-go/v2/cloudstack"
 	"github.com/hashicorp/terraform/helper/schema"
@@ -30,13 +35,14 @@ func dataSourceCloudstackInstance() *schema.Resource {
 	return &schema.Resource{
 		Read: dataSourceCloudstackInstanceRead,
 		Schema: map[string]*schema.Schema{
+			"filter": dataSourceFiltersSchema(),
 
+			//Computed values
 			"instance_id": {
 				Type:     schema.TypeString,
-				Required: true,
+				Computed: true,
 			},
 
-			// Computed values
 			"account": {
 				Type:     schema.TypeString,
 				Computed: true,
@@ -62,10 +68,7 @@ func dataSourceCloudstackInstance() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
-			// "ip_address": {
-			// 	Type:     schema.TypeString,
-			// 	Computed: true,
-			// },
+			"tags": tagsSchema(),
 		},
 	}
 }
@@ -75,37 +78,96 @@ func dataSourceCloudstackInstanceRead(d *schema.ResourceData, meta interface{}) 
 
 	cs := meta.(*cloudstack.CloudStackClient)
 	p := cs.VirtualMachine.NewListVirtualMachinesParams()
-	instance_id := d.Get("instance_id").(string)
 
-	log.Printf("Instance ID =====================> %v", instance_id)
+	csInstances, err := cs.VirtualMachine.ListVirtualMachines(p)
+	if err != nil {
+		return fmt.Errorf("Failed to list instances: %s", err)
+	}
 
-	csInstances, _ := cs.VirtualMachine.ListVirtualMachines(p)
+	filters := d.Get("filter")
+	var instances []*cloudstack.VirtualMachine
 
-	for _, instance := range csInstances.VirtualMachines {
-		log.Printf("Instance ===============> %v", instance)
-		if instance.Id == instance_id {
-			d.SetId(instance.Id)
-			d.Set("instance_id", instance.Id)
-			d.Set("account", instance.Account)
-			d.Set("created", instance.Created)
-			d.Set("display_name", instance.Displayname)
-			d.Set("state", instance.State)
-			d.Set("host_id", instance.Hostid)
-			d.Set("zone_id", instance.Zoneid)
+	for _, i := range csInstances.VirtualMachines {
+		match, err := applyInstanceFilters(i, filters.(*schema.Set))
+		if err != nil {
+			return err
+		}
 
-			log.Printf("=========================================================================")
-
-			log.Printf("Instance-ID being Set To: ===============> %v", d.Get("instance_id").(string))
-			log.Printf("Instance-Created being Set To: ===============> %v", d.Get("created").(string))
-			log.Printf("Instance-Display-Name being Set To: ===============> %v", d.Get("display_name").(string))
-			log.Printf("Instance-State being Set To: ===============> %v", d.Get("state").(string))
-			log.Printf("Instance-Host-ID being Set To: ===============> %v", d.Get("host_id").(string))
-			log.Printf("Instance-Zone-ID being Set To: ===============> %v", d.Get("zone_id").(string))
-
-			log.Printf("=========================================================================")
+		if match {
+			instances = append(instances, i)
 		}
 	}
-	log.Printf("Instance Data Source Read Ended")
+
+	if len(instances) == 0 {
+		return fmt.Errorf("No instance is matching with the specified regex")
+	}
+
+	instance, err := latestInstance(instances)
+	if err != nil {
+		return err
+	}
+	log.Printf("[DEBUG] Selected instances: %s\n", instance.Displayname)
+
+	return instanceDescriptionAttributes(d, instance)
+}
+
+func instanceDescriptionAttributes(d *schema.ResourceData, instance *cloudstack.VirtualMachine) error {
+	d.SetId(instance.Id)
+	d.Set("instance_id", instance.Id)
+	d.Set("account", instance.Account)
+	d.Set("created", instance.Created)
+	d.Set("display_name", instance.Displayname)
+	d.Set("state", instance.State)
+	d.Set("host_id", instance.Hostid)
+	d.Set("zone_id", instance.Zoneid)
+
+	tags := make(map[string]interface{})
+	for _, tag := range instance.Tags {
+		tags[tag.Key] = tag.Value
+	}
+	d.Set("tags", tags)
 
 	return nil
+}
+
+func latestInstance(instances []*cloudstack.VirtualMachine) (*cloudstack.VirtualMachine, error) {
+	var latest time.Time
+	var instance *cloudstack.VirtualMachine
+
+	for _, i := range instances {
+		created, err := time.Parse("2006-01-02T15:04:05-0700", i.Created)
+		if err != nil {
+			return nil, fmt.Errorf("Failed to parse creation date of an instance: %s", err)
+		}
+
+		if created.After(latest) {
+			latest = created
+			instance = i
+		}
+	}
+
+	return instance, nil
+}
+
+func applyInstanceFilters(instance *cloudstack.VirtualMachine, filters *schema.Set) (bool, error) {
+	var instanceJSON map[string]interface{}
+	i, _ := json.Marshal(instance)
+	err := json.Unmarshal(i, &instanceJSON)
+	if err != nil {
+		return false, err
+	}
+	for _, f := range filters.List() {
+		m := f.(map[string]interface{})
+		r, err := regexp.Compile(m["value"].(string))
+		if err != nil {
+			return false, fmt.Errorf("Invalid regex: %s", err)
+		}
+		updatedName := strings.ReplaceAll(m["name"].(string), "_", "")
+		instanceField := instanceJSON[updatedName].(string)
+		if !r.MatchString(instanceField) {
+			return false, nil
+		}
+
+	}
+	return true, nil
 }
