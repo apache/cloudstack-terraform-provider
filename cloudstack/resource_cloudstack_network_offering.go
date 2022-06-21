f4d6d3e7c7483de40312d3929a40e9d632e53546
damans227 <damans227@gmail.com>
2022-06-20 20:25:02 -0400

    complete network offering resource type

--- cloudstack/resource_cloudstack_network_offering.go
+++ cloudstack/resource_cloudstack_network_offering.go
@@ -20,6 +20,7 @@
 package cloudstack
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/apache/cloudstack-go/v2/cloudstack"
@@ -82,13 +83,132 @@ func resourceCloudStackNetworkOfferingCreate(d *schema.ResourceData, meta interf
 }
 
 func resourceCloudStackNetworkOfferingUpdate(d *schema.ResourceData, meta interface{}) error {
-	return nil
+	cs := meta.(*cloudstack.CloudStackClient)
+	d.Partial(true)
+
+	name := d.Get("name").(string)
+
+	// Check if the name is changed and if so, update the network offering
+	if d.HasChange("name") {
+		log.Printf("[DEBUG] Name changed for %s, starting update", name)
+
+		// Create a new parameter struct
+		p := cs.NetworkOffering.NewUpdateNetworkOfferingParams()
+
+		// Set the new name
+		p.SetName(d.Get("name").(string))
+
+		// Update the name
+		_, err := cs.NetworkOffering.UpdateNetworkOffering(p)
+		if err != nil {
+			return fmt.Errorf(
+				"Error updating the name for network offering %s: %s", name, err)
+		}
+
+		d.SetPartial("name")
+	}
+
+	// Check if the display text is changed and if so, update the virtual machine
+	if d.HasChange("display_text") {
+		log.Printf("[DEBUG] Display text changed for %s, starting update", name)
+
+		// Create a new parameter struct
+		p := cs.NetworkOffering.NewUpdateNetworkOfferingParams()
+
+		// Set the new display text
+		p.SetName(d.Get("display_text").(string))
+
+		// Update the display text
+		_, err := cs.NetworkOffering.UpdateNetworkOffering(p)
+		if err != nil {
+			return fmt.Errorf(
+				"Error updating the display text for network offering %s: %s", name, err)
+		}
+
+		d.SetPartial("display_text")
+	}
+
+	// Check if the guest ip type is changed and if so, update the virtual machine
+	if d.HasChange("guest_ip_type") {
+		log.Printf("[DEBUG] Guest ip type changed for %s, starting update", name)
+
+		// Create a new parameter struct
+		p := cs.NetworkOffering.NewUpdateNetworkOfferingParams()
+
+		// Set the new uest ip type
+		p.SetName(d.Get("guest_ip_type").(string))
+
+		// Update the uest ip type
+		_, err := cs.NetworkOffering.UpdateNetworkOffering(p)
+		if err != nil {
+			return fmt.Errorf(
+				"Error updating the guest ip type for network offering %s: %s", name, err)
+		}
+
+		d.SetPartial("guest_ip_type")
+	}
+
+	// Check if the traffic type is changed and if so, update the virtual machine
+	if d.HasChange("traffic_type") {
+		log.Printf("[DEBUG] Traffic type changed for %s, starting update", name)
+
+		// Create a new parameter struct
+		p := cs.NetworkOffering.NewUpdateNetworkOfferingParams()
+
+		// Set the new traffic type
+		p.SetName(d.Get("traffic_type").(string))
+
+		// Update the traffic type
+		_, err := cs.NetworkOffering.UpdateNetworkOffering(p)
+		if err != nil {
+			return fmt.Errorf(
+				"Error updating the traffic type for network offering %s: %s", name, err)
+		}
+
+		d.SetPartial("traffic_type")
+	}
+
+	d.Partial(false)
+
+	return resourceCloudStackInstanceRead(d, meta)
 }
 
 func resourceCloudStackNetworkOfferingDelete(d *schema.ResourceData, meta interface{}) error {
+	cs := meta.(*cloudstack.CloudStackClient)
+
+	// Create a new parameter struct
+	p := cs.NetworkOffering.NewDeleteNetworkOfferingParams(d.Id())
+	_, err := cs.NetworkOffering.DeleteNetworkOffering(p)
+
+	if err != nil {
+		return fmt.Errorf("Error deleting Kubernetes Cluster: %s", err)
+	}
+
 	return nil
 }
 
 func resourceCloudStackNetworkOfferingRead(d *schema.ResourceData, meta interface{}) error {
+	cs := meta.(*cloudstack.CloudStackClient)
+	log.Printf("[DEBUG] Retrieving Network Offering %s", d.Get("name").(string))
+
+	// Get the Network Offering details
+	n, count, err := cs.NetworkOffering.GetNetworkOfferingByName(d.Get("name").(string))
+
+	if err != nil {
+		if count == 0 {
+			log.Printf("[DEBUG] Network Offering %s does no longer exist", d.Get("name").(string))
+			d.SetId("")
+			return nil
+		}
+
+		return err
+	}
+
+	d.SetId(n.Id)
+	d.Set("name", n.Name)
+	d.Set("display_text", n.Displaytext)
+	d.Set("guest_ip_type", n.Guestiptype)
+	d.Set("traffic_type", n.Traffictype)
+
 	return nil
 }
