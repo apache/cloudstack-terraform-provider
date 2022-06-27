2a36da546f4de550a38b725b0e3d60144e6ee6ff
damans227 <damans227@gmail.com>
2022-06-27 19:22:03 -0400

    create disk offering resource

---
+++ cloudstack/resource_cloudstack_disk_offering.go
@@ -0,0 +1,53 @@
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+package cloudstack
+
+import "github.com/hashicorp/terraform/helper/schema"
+
+func resourceCloudstackDiskOffering() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceCloudStackDiskOfferingCreate,
+		Read:   resourceCloudStackDiskOfferingRead,
+		Update: resourceCloudStackDiskOfferingUpdate,
+		Delete: resourceCloudStackDiskOfferingDelete,
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"display_text": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"disk_size": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourceCloudStackDiskOfferingCreate(d *schema.ResourceData, meta interface{}) error { return nil }
+
+func resourceCloudStackDiskOfferingRead(d *schema.ResourceData, meta interface{}) error { return nil }
+
+func resourceCloudStackDiskOfferingUpdate(d *schema.ResourceData, meta interface{}) error { return nil }
+
+func resourceCloudStackDiskOfferingDelete(d *schema.ResourceData, meta interface{}) error { return nil }
