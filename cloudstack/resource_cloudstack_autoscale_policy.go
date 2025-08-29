65495540fa211752c3c76e8bc77302ecff6bb6a4
Pearl Dsilva <pearl1594@gmail.com>
2025-08-29 11:59:47 -0400

    Add support for Autoscale VM groups

---
+++ cloudstack/resource_cloudstack_autoscale_policy.go
@@ -0,0 +1,195 @@
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+package cloudstack
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/apache/cloudstack-go/v2/cloudstack"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceCloudStackAutoScalePolicy() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceCloudStackAutoScalePolicyCreate,
+		Read:   resourceCloudStackAutoScalePolicyRead,
+		Update: resourceCloudStackAutoScalePolicyUpdate,
+		Delete: resourceCloudStackAutoScalePolicyDelete,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "the name of the autoscale policy",
+			},
+			"action": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "the action to be executed if all the conditions evaluate to true for the specified duration",
+				ForceNew:    true,
+			},
+			"duration": {
+				Type:        schema.TypeInt,
+				Required:    true,
+				Description: "the duration in which the conditions have to be true before action is taken",
+			},
+			"quiet_time": {
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Description: "the cool down period in which the policy should not be evaluated after the action has been taken",
+			},
+			"condition_ids": {
+				Type:        schema.TypeSet,
+				Elem:        &schema.Schema{Type: schema.TypeString},
+				Required:    true,
+				Description: "the list of IDs of the conditions that are being evaluated on every interval",
+			},
+		},
+	}
+}
+
+func resourceCloudStackAutoScalePolicyCreate(d *schema.ResourceData, meta interface{}) error {
+	cs := meta.(*cloudstack.CloudStackClient)
+
+	action := d.Get("action").(string)
+	duration := d.Get("duration").(int)
+
+	conditionIds := []string{}
+	if v, ok := d.GetOk("condition_ids"); ok {
+		conditionSet := v.(*schema.Set)
+		for _, id := range conditionSet.List() {
+			conditionIds = append(conditionIds, id.(string))
+		}
+	}
+
+	p := cs.AutoScale.NewCreateAutoScalePolicyParams(action, conditionIds, duration)
+
+	if v, ok := d.GetOk("name"); ok {
+		p.SetName(v.(string))
+	}
+	if v, ok := d.GetOk("quiet_time"); ok {
+		p.SetQuiettime(v.(int))
+	}
+
+	log.Printf("[DEBUG] Creating autoscale policy")
+	resp, err := cs.AutoScale.CreateAutoScalePolicy(p)
+	if err != nil {
+		return fmt.Errorf("Error creating autoscale policy: %s", err)
+	}
+
+	d.SetId(resp.Id)
+	log.Printf("[DEBUG] Autoscale policy created with ID: %s", resp.Id)
+
+	return resourceCloudStackAutoScalePolicyRead(d, meta)
+}
+
+func resourceCloudStackAutoScalePolicyRead(d *schema.ResourceData, meta interface{}) error {
+	cs := meta.(*cloudstack.CloudStackClient)
+
+	p := cs.AutoScale.NewListAutoScalePoliciesParams()
+	p.SetId(d.Id())
+
+	resp, err := cs.AutoScale.ListAutoScalePolicies(p)
+	if err != nil {
+		return fmt.Errorf("Error retrieving autoscale policy: %s", err)
+	}
+
+	if resp.Count == 0 {
+		log.Printf("[DEBUG] Autoscale policy %s no longer exists", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	policy := resp.AutoScalePolicies[0]
+	d.Set("name", policy.Name)
+	d.Set("action", policy.Action)
+	d.Set("duration", policy.Duration)
+	d.Set("quiet_time", policy.Quiettime)
+
+	conditionIds := schema.NewSet(schema.HashString, []interface{}{})
+	for _, conditionId := range policy.Conditions {
+		conditionIds.Add(conditionId)
+	}
+	d.Set("condition_ids", conditionIds)
+
+	return nil
+}
+
+func resourceCloudStackAutoScalePolicyUpdate(d *schema.ResourceData, meta interface{}) error {
+	cs := meta.(*cloudstack.CloudStackClient)
+
+	if d.HasChange("name") || d.HasChange("condition_ids") || d.HasChange("duration") || d.HasChange("quiet_time") {
+		log.Printf("[DEBUG] Updating autoscale policy: %s", d.Id())
+
+		p := cs.AutoScale.NewUpdateAutoScalePolicyParams(d.Id())
+
+		if d.HasChange("name") {
+			if v, ok := d.GetOk("name"); ok {
+				p.SetName(v.(string))
+			}
+		}
+
+		if d.HasChange("duration") {
+			duration := d.Get("duration").(int)
+			p.SetDuration(duration)
+		}
+
+		if d.HasChange("quiet_time") {
+			if v, ok := d.GetOk("quiet_time"); ok {
+				p.SetQuiettime(v.(int))
+			}
+		}
+
+		if d.HasChange("condition_ids") {
+			conditionIds := []string{}
+			if v, ok := d.GetOk("condition_ids"); ok {
+				conditionSet := v.(*schema.Set)
+				for _, id := range conditionSet.List() {
+					conditionIds = append(conditionIds, id.(string))
+				}
+			}
+			p.SetConditionids(conditionIds)
+		}
+
+		_, err := cs.AutoScale.UpdateAutoScalePolicy(p)
+		if err != nil {
+			return fmt.Errorf("Error updating autoscale policy: %s", err)
+		}
+
+		log.Printf("[DEBUG] Autoscale policy updated successfully: %s", d.Id())
+	}
+
+	return resourceCloudStackAutoScalePolicyRead(d, meta)
+}
+
+func resourceCloudStackAutoScalePolicyDelete(d *schema.ResourceData, meta interface{}) error {
+	cs := meta.(*cloudstack.CloudStackClient)
+
+	p := cs.AutoScale.NewDeleteAutoScalePolicyParams(d.Id())
+
+	log.Printf("[DEBUG] Deleting autoscale policy: %s", d.Id())
+	_, err := cs.AutoScale.DeleteAutoScalePolicy(p)
+	if err != nil {
+		return fmt.Errorf("Error deleting autoscale policy: %s", err)
+	}
+
+	return nil
+}
---
+++ cloudstack/resource_cloudstack_autoscale_vm_group.go
@@ -0,0 +1,286 @@
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+package cloudstack
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/apache/cloudstack-go/v2/cloudstack"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceCloudStackAutoScaleVMGroup() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceCloudStackAutoScaleVMGroupCreate,
+		Read:   resourceCloudStackAutoScaleVMGroupRead,
+		Update: resourceCloudStackAutoScaleVMGroupUpdate,
+		Delete: resourceCloudStackAutoScaleVMGroupDelete,
+
+		Schema: map[string]*schema.Schema{
+			"lbrule_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "the ID of the load balancer rule",
+			},
+
+			"name": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "the name of the autoscale vmgroup",
+			},
+
+			"min_members": {
+				Type:        schema.TypeInt,
+				Required:    true,
+				Description: "the minimum number of members in the vmgroup, the number of instances in the vm group will be equal to or more than this number",
+			},
+
+			"max_members": {
+				Type:        schema.TypeInt,
+				Required:    true,
+				Description: "the maximum number of members in the vmgroup, The number of instances in the vm group will be equal to or less than this number",
+			},
+
+			"interval": {
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Description: "the frequency in which the performance counters to be collected",
+			},
+
+			"scaleup_policy_ids": {
+				Type:        schema.TypeSet,
+				Elem:        &schema.Schema{Type: schema.TypeString},
+				Required:    true,
+				Description: "list of scaleup autoscale policies",
+			},
+
+			"scaledown_policy_ids": {
+				Type:        schema.TypeSet,
+				Elem:        &schema.Schema{Type: schema.TypeString},
+				Required:    true,
+				Description: "list of scaledown autoscale policies",
+			},
+
+			"vm_profile_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "the autoscale profile that contains information about the vms in the vm group",
+			},
+
+			"display": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Description: "an optional field, whether to the display the group to the end user or not",
+			},
+		},
+	}
+}
+
+func resourceCloudStackAutoScaleVMGroupCreate(d *schema.ResourceData, meta interface{}) error {
+	cs := meta.(*cloudstack.CloudStackClient)
+
+	lbruleid := d.Get("lbrule_id").(string)
+	minmembers := d.Get("min_members").(int)
+	maxmembers := d.Get("max_members").(int)
+	vmprofileid := d.Get("vm_profile_id").(string)
+
+	scaleUpPolicyIds := []string{}
+	if v, ok := d.GetOk("scaleup_policy_ids"); ok {
+		scaleUpSet := v.(*schema.Set)
+		for _, id := range scaleUpSet.List() {
+			scaleUpPolicyIds = append(scaleUpPolicyIds, id.(string))
+		}
+	}
+
+	scaleDownPolicyIds := []string{}
+	if v, ok := d.GetOk("scaledown_policy_ids"); ok {
+		scaleDownSet := v.(*schema.Set)
+		for _, id := range scaleDownSet.List() {
+			scaleDownPolicyIds = append(scaleDownPolicyIds, id.(string))
+		}
+	}
+
+	p := cs.AutoScale.NewCreateAutoScaleVmGroupParams(lbruleid, maxmembers, minmembers, scaleDownPolicyIds, scaleUpPolicyIds, vmprofileid)
+
+	if v, ok := d.GetOk("name"); ok {
+		p.SetName(v.(string))
+	}
+
+	if v, ok := d.GetOk("interval"); ok {
+		p.SetInterval(v.(int))
+	}
+
+	if v, ok := d.GetOk("display"); ok {
+		p.SetFordisplay(v.(bool))
+	}
+
+	log.Printf("[DEBUG] Creating autoscale VM group")
+	resp, err := cs.AutoScale.CreateAutoScaleVmGroup(p)
+	if err != nil {
+		return fmt.Errorf("Error creating autoscale VM group: %s", err)
+	}
+
+	d.SetId(resp.Id)
+	log.Printf("[DEBUG] Autoscale VM group created with ID: %s", resp.Id)
+
+	return resourceCloudStackAutoScaleVMGroupRead(d, meta)
+}
+
+func resourceCloudStackAutoScaleVMGroupRead(d *schema.ResourceData, meta interface{}) error {
+	cs := meta.(*cloudstack.CloudStackClient)
+
+	p := cs.AutoScale.NewListAutoScaleVmGroupsParams()
+	p.SetId(d.Id())
+
+	resp, err := cs.AutoScale.ListAutoScaleVmGroups(p)
+	if err != nil {
+		return fmt.Errorf("Error retrieving autoscale VM group: %s", err)
+	}
+
+	if resp.Count == 0 {
+		log.Printf("[DEBUG] Autoscale VM group %s no longer exists", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	group := resp.AutoScaleVmGroups[0]
+	d.Set("name", group.Name)
+	d.Set("lbrule_id", group.Lbruleid)
+	d.Set("min_members", group.Minmembers)
+	d.Set("max_members", group.Maxmembers)
+	d.Set("interval", group.Interval)
+	d.Set("vm_profile_id", group.Vmprofileid)
+	d.Set("display", group.Fordisplay)
+
+	scaleUpPolicyIds := schema.NewSet(schema.HashString, []interface{}{})
+	if group.Scaleuppolicies != nil {
+		for _, policyId := range group.Scaleuppolicies {
+			scaleUpPolicyIds.Add(policyId)
+		}
+	}
+	d.Set("scaleup_policy_ids", scaleUpPolicyIds)
+
+	scaleDownPolicyIds := schema.NewSet(schema.HashString, []interface{}{})
+	if group.Scaledownpolicies != nil {
+		for _, policyId := range group.Scaledownpolicies {
+			scaleDownPolicyIds.Add(policyId)
+		}
+	}
+	d.Set("scaledown_policy_ids", scaleDownPolicyIds)
+
+	return nil
+}
+
+func resourceCloudStackAutoScaleVMGroupUpdate(d *schema.ResourceData, meta interface{}) error {
+	cs := meta.(*cloudstack.CloudStackClient)
+
+	if d.HasChange("name") || d.HasChange("min_members") || d.HasChange("max_members") ||
+		d.HasChange("interval") || d.HasChange("scaleup_policy_ids") ||
+		d.HasChange("scaledown_policy_ids") || d.HasChange("display") {
+
+		log.Printf("[DEBUG] Updating autoscale VM group: %s", d.Id())
+
+		p := cs.AutoScale.NewUpdateAutoScaleVmGroupParams(d.Id())
+
+		if d.HasChange("name") {
+			if v, ok := d.GetOk("name"); ok {
+				p.SetName(v.(string))
+			}
+		}
+
+		if d.HasChange("min_members") {
+			minmembers := d.Get("min_members").(int)
+			p.SetMinmembers(minmembers)
+		}
+
+		if d.HasChange("max_members") {
+			maxmembers := d.Get("max_members").(int)
+			p.SetMaxmembers(maxmembers)
+		}
+
+		if d.HasChange("interval") {
+			if v, ok := d.GetOk("interval"); ok {
+				p.SetInterval(v.(int))
+			}
+		}
+
+		if d.HasChange("scaleup_policy_ids") {
+			scaleUpPolicyIds := []string{}
+			if v, ok := d.GetOk("scaleup_policy_ids"); ok {
+				scaleUpSet := v.(*schema.Set)
+				for _, id := range scaleUpSet.List() {
+					scaleUpPolicyIds = append(scaleUpPolicyIds, id.(string))
+				}
+			}
+			p.SetScaleuppolicyids(scaleUpPolicyIds)
+		}
+
+		if d.HasChange("scaledown_policy_ids") {
+			scaleDownPolicyIds := []string{}
+			if v, ok := d.GetOk("scaledown_policy_ids"); ok {
+				scaleDownSet := v.(*schema.Set)
+				for _, id := range scaleDownSet.List() {
+					scaleDownPolicyIds = append(scaleDownPolicyIds, id.(string))
+				}
+			}
+			p.SetScaledownpolicyids(scaleDownPolicyIds)
+		}
+
+		if d.HasChange("display") {
+			if v, ok := d.GetOk("display"); ok {
+				p.SetFordisplay(v.(bool))
+			}
+		}
+
+		_, err := cs.AutoScale.UpdateAutoScaleVmGroup(p)
+		if err != nil {
+			return fmt.Errorf("Error updating autoscale VM group: %s", err)
+		}
+
+		log.Printf("[DEBUG] Autoscale VM group updated successfully: %s", d.Id())
+	}
+
+	return resourceCloudStackAutoScaleVMGroupRead(d, meta)
+}
+
+func resourceCloudStackAutoScaleVMGroupDelete(d *schema.ResourceData, meta interface{}) error {
+	cs := meta.(*cloudstack.CloudStackClient)
+
+	p := cs.AutoScale.NewDeleteAutoScaleVmGroupParams(d.Id())
+
+	log.Printf("[DEBUG] Deleting autoscale VM group: %s", d.Id())
+	_, err := cs.AutoScale.DeleteAutoScaleVmGroup(p)
+	if err != nil {
+		// This is a very poor way to be told the ID does no longer exist :(
+		if strings.Contains(err.Error(), fmt.Sprintf(
+			"Invalid parameter id value=%s due to incorrect long value format, "+
+				"or entity does not exist", d.Id())) {
+			return nil
+		}
+
+		return fmt.Errorf("Error deleting autoscale VM group: %s", err)
+	}
+
+	return nil
+}
