b70cce47ada9c3d2b2ae03459b97226afd464e7f
poddm <8801231+poddm@users.noreply.github.com>
2025-09-10 12:47:08 -0700

    rebase

--- cloudstack/resource_cloudstack_traffic_type.go
+++ cloudstack/resource_cloudstack_traffic_type.go
@@ -39,46 +39,60 @@ func resourceCloudStackTrafficType() *schema.Resource {
 		},
 
 		Schema: map[string]*schema.Schema{
-			"physical_network_id": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
-			},
-
-			"type": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
-			},
-
-			"kvm_network_label": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-
-			"vlan": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-
-			"xen_network_label": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-
-			"vmware_network_label": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-
 			"hyperv_network_label": {
-				Type:     schema.TypeString,
-				Optional: true,
+				Description: "The network name label of the physical device dedicated to this traffic on a Hyperv host",
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    true,
 			},
-
+			"isolation_method": {
+				Description: "Used if physical network has multiple isolation types and traffic type is public. Choose which isolation method. Valid options currently 'vlan' or 'vxlan', defaults to 'vlan'.",
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    true,
+			},
+			"kvm_network_label": {
+				Description: "The network name label of the physical device dedicated to this traffic on a KVM host",
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    true,
+			},
 			"ovm3_network_label": {
-				Type:     schema.TypeString,
-				Optional: true,
+				Description: "The network name of the physical device dedicated to this traffic on an OVM3 host",
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    true,
+			},
+			"physical_network_id": {
+				Description: "the Physical Network ID",
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+			},
+			"traffic_type": {
+				Description:  "the trafficType to be added to the physical network",
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validateTrafficType,
+			},
+			"vlan": {
+				Description: "The VLAN id to be used for Management traffic by VMware host",
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    true,
+			},
+			"vmware_network_label": {
+				Description: "The network name label of the physical device dedicated to this traffic on a VMware host",
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    true,
+			},
+			"xen_network_label": {
+				Description: "The network name label of the physical device dedicated to this traffic on a XenServer host",
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    true,
 			},
 		},
 	}
@@ -96,6 +110,10 @@ func resourceCloudStackTrafficTypeCreate(d *schema.ResourceData, meta interface{
 	// Set optional parameters
 	if kvmNetworkLabel, ok := d.GetOk("kvm_network_label"); ok {
 		p.SetKvmnetworklabel(kvmNetworkLabel.(string))
+	}
+
+	if v, ok := d.GetOk("isolation_method"); ok {
+		p.SetIsolationmethod(v.(string))
 	}
 
 	if vlan, ok := d.GetOk("vlan"); ok {
@@ -125,6 +143,14 @@ func resourceCloudStackTrafficTypeCreate(d *schema.ResourceData, meta interface{
 	}
 
 	d.SetId(r.Id)
+
+	d.Set("physical_network_id", d.Get("physical_network_id").(string))
+	d.Set("hyperv_network_label", r.Hypervnetworklabel)
+	d.Set("kvm_network_label", r.Kvmnetworklabel)
+	d.Set("ovm3_network_label", r.Ovm3networklabel)
+	d.Set("traffic_type", r.Traffictype)
+	d.Set("vmware_network_label", r.Vmwarenetworklabel)
+	d.Set("xen_network_label", r.Xennetworklabel)
 
 	return resourceCloudStackTrafficTypeRead(d, meta)
 }
@@ -198,10 +224,17 @@ func resourceCloudStackTrafficTypeUpdate(d *schema.ResourceData, meta interface{
 	// so we can't update the VLAN
 
 	// Update the traffic type
-	_, err := cs.Usage.UpdateTrafficType(p)
+	r, err := cs.Usage.UpdateTrafficType(p)
 	if err != nil {
 		return fmt.Errorf("Error updating traffic type %s: %s", d.Get("type").(string), err)
 	}
+
+	d.Set("hyperv_network_label", r.Hypervnetworklabel)
+	d.Set("kvm_network_label", r.Kvmnetworklabel)
+	d.Set("ovm3_network_label", r.Ovm3networklabel)
+	d.Set("traffic_type", r.Traffictype)
+	d.Set("vmware_network_label", r.Vmwarenetworklabel)
+	d.Set("xen_network_label", r.Xennetworklabel)
 
 	return resourceCloudStackTrafficTypeRead(d, meta)
 }
@@ -279,199 +312,6 @@ func resourceCloudStackTrafficTypeImport(d *schema.ResourceData, meta interface{
 	}
 
 	return nil, fmt.Errorf("could not find physical network for traffic type %s", d.Id())
-}
-
-//
-// Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements.  See the NOTICE file
-// distributed with this work for additional information
-// regarding copyright ownership.  The ASF licenses this file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
-//
-
-package cloudstack
-
-import (
-	"fmt"
-
-	"github.com/apache/cloudstack-go/v2/cloudstack"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-)
-
-func resourceCloudStackTrafficType() *schema.Resource {
-	return &schema.Resource{
-		Create: resourceCloudStackTrafficTypeCreate,
-		Read:   resourceCloudStackTrafficTypeRead,
-		// Update: resourceCloudStackTrafficTypeUpdate,
-		Delete: resourceCloudStackTrafficTypeDelete,
-		Schema: map[string]*schema.Schema{
-			"hyperv_network_label": {
-				Description: "The network name label of the physical device dedicated to this traffic on a Hyperv host",
-				Type:        schema.TypeString,
-				Optional:    true,
-				ForceNew:    true,
-			},
-			"isolation_method": {
-				Description: "Used if physical network has multiple isolation types and traffic type is public. Choose which isolation method. Valid options currently 'vlan' or 'vxlan', defaults to 'vlan'.",
-				Type:        schema.TypeString,
-				Optional:    true,
-				ForceNew:    true,
-			},
-			"kvm_network_label": {
-				Description: "The network name label of the physical device dedicated to this traffic on a KVM host",
-				Type:        schema.TypeString,
-				Optional:    true,
-				ForceNew:    true,
-			},
-			"ovm3_network_label": {
-				Description: "The network name of the physical device dedicated to this traffic on an OVM3 host",
-				Type:        schema.TypeString,
-				Optional:    true,
-				ForceNew:    true,
-			},
-			"physical_network_id": {
-				Description: "the Physical Network ID",
-				Type:        schema.TypeString,
-				Required:    true,
-				ForceNew:    true,
-			},
-			"traffic_type": {
-				Description:  "the trafficType to be added to the physical network",
-				Type:         schema.TypeString,
-				Required:     true,
-				ForceNew:     true,
-				ValidateFunc: validateTrafficType,
-			},
-			"vlan": {
-				Description: "The VLAN id to be used for Management traffic by VMware host",
-				Type:        schema.TypeString,
-				Optional:    true,
-				ForceNew:    true,
-			},
-			"vmware_network_label": {
-				Description: "The network name label of the physical device dedicated to this traffic on a VMware host",
-				Type:        schema.TypeString,
-				Optional:    true,
-				ForceNew:    true,
-			},
-			"xen_network_label": {
-				Description: "The network name label of the physical device dedicated to this traffic on a XenServer host",
-				Type:        schema.TypeString,
-				Optional:    true,
-				ForceNew:    true,
-			},
-		},
-	}
-}
-
-func resourceCloudStackTrafficTypeCreate(d *schema.ResourceData, meta interface{}) error {
-	cs := meta.(*cloudstack.CloudStackClient)
-
-	p := cs.Usage.NewAddTrafficTypeParams(d.Get("physical_network_id").(string), d.Get("traffic_type").(string))
-	if v, ok := d.GetOk("hyperv_network_label"); ok {
-		p.SetHypervnetworklabel(v.(string))
-	}
-	if v, ok := d.GetOk("isolation_method"); ok {
-		p.SetIsolationmethod(v.(string))
-	}
-	if v, ok := d.GetOk("kvm_network_label"); ok {
-		p.SetKvmnetworklabel(v.(string))
-	}
-	if v, ok := d.GetOk("ovm3_network_label"); ok {
-		p.SetOvm3networklabel(v.(string))
-	}
-	if v, ok := d.GetOk("vlan"); ok {
-		p.SetVlan(v.(string))
-	}
-	if v, ok := d.GetOk("vmware_network_label"); ok {
-		p.SetVmwarenetworklabel(v.(string))
-	}
-	if v, ok := d.GetOk("xen_network_label"); ok {
-		p.SetXennetworklabel(v.(string))
-	}
-
-	r, err := cs.Usage.AddTrafficType(p)
-	if err != nil {
-		return err
-	}
-	d.SetId(r.Id)
-	d.Set("physical_network_id", d.Get("physical_network_id").(string))
-
-	//
-	d.Set("hyperv_network_label", r.Hypervnetworklabel)
-	d.Set("kvm_network_label", r.Kvmnetworklabel)
-	d.Set("ovm3_network_label", r.Ovm3networklabel)
-	d.Set("traffic_type", r.Traffictype)
-	d.Set("vmware_network_label", r.Vmwarenetworklabel)
-	d.Set("xen_network_label", r.Xennetworklabel)
-
-	return resourceCloudStackTrafficTypeRead(d, meta)
-}
-
-func resourceCloudStackTrafficTypeRead(d *schema.ResourceData, meta interface{}) error {
-	// Nothing to read.  While these fields are returned by the API
-	// they are not documented in the client or ListApi spec.
-	// see https://github.com/apache/cloudstack/issues/7837
-
-	return nil
-}
-
-func resourceCloudStackTrafficTypeUpdate(d *schema.ResourceData, meta interface{}) error {
-	// All fields are ForceNew or Computed w/out Optional, Update is superfluous
-	cs := meta.(*cloudstack.CloudStackClient)
-
-	p := cs.Usage.NewUpdateTrafficTypeParams(d.Id())
-	if v, ok := d.GetOk("hyperv_network_label"); ok {
-		p.SetHypervnetworklabel(v.(string))
-	}
-	if v, ok := d.GetOk("kvm_network_label"); ok {
-		p.SetKvmnetworklabel(v.(string))
-	}
-	if v, ok := d.GetOk("ovm3_network_label"); ok {
-		p.SetOvm3networklabel(v.(string))
-	}
-	if v, ok := d.GetOk("vmware_network_label"); ok {
-		p.SetVmwarenetworklabel(v.(string))
-	}
-	if v, ok := d.GetOk("xen_network_label"); ok {
-		p.SetXennetworklabel(v.(string))
-	}
-
-	r, err := cs.Usage.UpdateTrafficType(p)
-	if err != nil {
-		return err
-	}
-
-	d.Set("hyperv_network_label", r.Hypervnetworklabel)
-	d.Set("kvm_network_label", r.Kvmnetworklabel)
-	d.Set("ovm3_network_label", r.Ovm3networklabel)
-	d.Set("traffic_type", r.Traffictype)
-	d.Set("vmware_network_label", r.Vmwarenetworklabel)
-	d.Set("xen_network_label", r.Xennetworklabel)
-
-	return resourceCloudStackTrafficTypeRead(d, meta)
-}
-
-func resourceCloudStackTrafficTypeDelete(d *schema.ResourceData, meta interface{}) error {
-	cs := meta.(*cloudstack.CloudStackClient)
-
-	_, err := cs.Usage.DeleteTrafficType(cs.Usage.NewDeleteTrafficTypeParams(d.Id()))
-	if err != nil {
-		return fmt.Errorf("Error deleting traffic type: %s", err)
-	}
-
-	return nil
 }
 
 func validateTrafficType(v interface{}, _ string) (warnings []string, errors []error) {
